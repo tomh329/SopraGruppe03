2b8f524cdf59426098a0614d8344d5634811a97f
Tom Holzkämper <holzkaempertom@gmail.com>
2021-05-18 10:42:16 +0200

    Projekt erstellt

---
+++ Bedrohungsidentifizierungssystem/src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/BedrohungsidentifizierungssystemApplication.java
@@ -0,0 +1,13 @@
+package de.gruppe3.bedrohungsidentifizierungssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedrohungsidentifizierungssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BedrohungsidentifizierungssystemApplication.class, args);
+    }
+
+}
---
+++ Bedrohungsidentifizierungssystem/src/test/java/de/gruppe3/bedrohungsidentifizierungssystem/BedrohungsidentifizierungssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gruppe3.bedrohungsidentifizierungssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedrohungsidentifizierungssystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
