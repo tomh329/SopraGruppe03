0bbe3c2e6f36d604a5fcdf543b66c307a04931d4
st168224 <st168224@stud.uni-stuttgart.de>
2021-06-03 12:54:06 +0200

    Testdata for Database

---
+++ Bedrohungsidentifizierungssystem/src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/service/DataLoader.java
@@ -0,0 +1,36 @@
+package de.gruppe3.bedrohungsidentifizierungssystem.service;
+
+import de.gruppe3.bedrohungsidentifizierungssystem.entity.Process;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataLoader.class);
+
+
+    @Autowired
+    private ProcessService processService;
+
+    /**
+     * Diese Methode wird zum Aufsetzen von Testdaten für die Datenbank verwendet werden. Die Methode wird immer dann
+     * ausgeführt, wenn der Spring Kontext initialisiert wurde, d.h. wenn Sie Ihren Server (neu-)starten.
+     */
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        logger.info("Initialisiere Datenbank mit Testdaten...");
+
+        // Initialisieren Sie Beispielobjekte und speichern Sie diese über Ihre Services
+
+        Process normalProcess = new Process();
+        normalProcess.setProcessName("NormalProzess");
+        normalProcess.setProtectionLevel("Hoch");
+        processService.saveProcess(normalProcess);
+
+    }
+}
