26ea9dbfa46d482a5275cd6bda0dccfd04f45bff
JanBorufka <st166722@stud.uni-stuttgart.de>
2021-06-19 22:01:32 +0200

    implemented first version of home site

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/HomeController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/HomeController.java
@@ -1,8 +1,11 @@
 package de.gruppe3.bedrohungsidentifizierungssystem.controller;
 
+import org.apache.tomcat.jni.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -14,8 +17,16 @@ public class HomeController {
      */
     @GetMapping("/")
     public String showHome(Model model) {
-        model.addAttribute("message", "Und hier sehen Sie ein ModelAttribut");
+        model.addAttribute("user", new User());
+
         return "home";
     }
+
+    @PostMapping("/login")
+    public String login(){
+        return "dashboard";
+
+    }
+
 
 }
