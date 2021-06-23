a50a31325f48cd65a2cfd5566750cfcac09c9257
JanBorufka <st166722@stud.uni-stuttgart.de>
2021-06-23 17:15:56 +0200

    implemented NavBar

---
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/DashboardController.java
@@ -0,0 +1,21 @@
+package de.gruppe3.bedrohungsidentifizierungssystem.controller;
+
+import org.apache.tomcat.jni.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+
+
+        return "dashboard";
+    }
+
+
+}
