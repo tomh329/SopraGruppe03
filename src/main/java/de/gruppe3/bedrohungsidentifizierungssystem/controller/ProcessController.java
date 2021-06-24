e62e54a98dc768e4fafffb8503124f7741141434
kalyoncubahar <bahar.kalyoncu@outlook.de>
2021-06-24 17:29:35 +0200

    Edit Funktion - Datenübergabe aus dem Modell nicht gelöst

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/ProcessController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/ProcessController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,4 +27,11 @@ public class ProcessController {
     public String create(){
         return "redirect:/editProcess";
     }
+
+    @GetMapping({"/process/{processId}"})
+    public String showEditProcess(Model model, @PathVariable Integer processId) {
+        model.addAttribute("editProcessId", processService.findProcessWithId(processId));
+        return "editProcess";
+    }
+
 }
