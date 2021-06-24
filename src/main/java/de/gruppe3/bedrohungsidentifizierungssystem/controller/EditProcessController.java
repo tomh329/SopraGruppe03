cc461e26484e36fedbb4ebc3ea7994155c4a51ed
kalyoncubahar <bahar.kalyoncu@outlook.de>
2021-06-24 20:45:23 +0200

    Zwischenstand

---
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/EditProcessController.java
@@ -0,0 +1,39 @@
+package de.gruppe3.bedrohungsidentifizierungssystem.controller;
+
+import de.gruppe3.bedrohungsidentifizierungssystem.repository.ComponentRepository;
+import de.gruppe3.bedrohungsidentifizierungssystem.service.ProcessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EditProcessController {
+
+    @GetMapping("/editProcess")
+    public String showEditProcess() {
+
+        return "editProcess";
+    }
+
+    @Autowired
+    private ProcessService processService;
+    @Autowired
+    ComponentRepository componentRepository;
+
+    @PostMapping("/editProcess")
+    public String create(@RequestParam(name = "processName") String processName,
+                         @RequestParam (name = "protectionLevel") int protectionLevel,
+                         @RequestParam (name = "componentId") int componentId){
+
+        processService.createProcess(processName, protectionLevel, componentId);
+
+        System.out.println(processName);
+        System.out.println(protectionLevel);
+        System.out.println(componentId);
+
+        return "redirect:/process";
+    }
+
+}
