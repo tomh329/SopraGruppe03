2fa8ba78d3501da8edb497e44e625c2c5dd46b4b
kalyoncubahar <bahar.kalyoncu@outlook.de>
2021-06-24 13:36:55 +0200

    Prozesstabelle angelegt - Inhaltbefüllung funktioniert nicht

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/ProcessController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/ProcessController.java
@@ -1,15 +1,10 @@
 package de.gruppe3.bedrohungsidentifizierungssystem.controller;
 
-import de.gruppe3.bedrohungsidentifizierungssystem.repository.ProcessRepository;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.ProcessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @Controller
 public class ProcessController {
