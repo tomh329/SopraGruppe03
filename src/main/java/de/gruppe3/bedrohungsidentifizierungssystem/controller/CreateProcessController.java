9c82b64b128de5885bdda5c92f974e938727f550
JanBorufka <st166722@stud.uni-stuttgart.de>
2021-06-25 22:18:35 +0200

    Implemented error handling prozess

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/CreateProcessController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/CreateProcessController.java
@@ -2,18 +2,23 @@ package de.gruppe3.bedrohungsidentifizierungssystem.controller;
 
 import de.gruppe3.bedrohungsidentifizierungssystem.repository.ComponentRepository;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.ProcessService;
+import de.gruppe3.bedrohungsidentifizierungssystem.entity.Process;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class CreateProcessController {
 
     @GetMapping("/createProcess")
-    public String showCreateProcess() {
-
+    public String showCreateProcess(Process process, BindingResult bindingResult, Model model) {
+        model.addAttribute("process",process);
         return "createProcess";
     }
 
@@ -24,8 +29,14 @@ public class CreateProcessController {
 
     @PostMapping("/createProcess")
     public String create(@RequestParam(name = "processName") String processName,
-                         @RequestParam(name = "protectionLevel") int protectionLevel) {
+                         @RequestParam(name = "protectionLevel") int protectionLevel,
+                         @Valid Process process, BindingResult bindingResult, Model model) {
 
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("process",process);
+            return "/createProcess";
+
+        }
 
         processService.createProcess(processName, protectionLevel);
 
