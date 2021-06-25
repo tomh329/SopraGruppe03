7eb7369437bd9466ccc2468170f5e1cd03e02edc
kalyoncubahar <bahar.kalyoncu@outlook.de>
2021-06-25 10:14:04 +0200

    user table added

---
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/UserController.java
@@ -0,0 +1,26 @@
+package de.gruppe3.bedrohungsidentifizierungssystem.controller;
+
+import de.gruppe3.bedrohungsidentifizierungssystem.service.ProcessService;
+import de.gruppe3.bedrohungsidentifizierungssystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/user")
+    public String showUser(Model model) {
+
+        model.addAttribute("users", userService.findAllUsers());
+        return "user";
+    }
+
+
+}
