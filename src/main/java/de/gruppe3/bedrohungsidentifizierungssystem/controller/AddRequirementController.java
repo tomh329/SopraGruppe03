6d060a500e4df63fda51b33240f85dfa32bfb9c8
Tom Holzkämper <holzkaempertom@gmail.com>
2021-07-08 19:04:22 +0200

    addRequirement optimized

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/AddRequirementController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/AddRequirementController.java
@@ -5,9 +5,11 @@ import de.gruppe3.bedrohungsidentifizierungssystem.entity.Component;
 import de.gruppe3.bedrohungsidentifizierungssystem.entity.Requirement;
 import de.gruppe3.bedrohungsidentifizierungssystem.repository.ComponentRepository;
 import de.gruppe3.bedrohungsidentifizierungssystem.repository.RequirementRepository;
+import de.gruppe3.bedrohungsidentifizierungssystem.service.ComponentService;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.RequirementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,19 +20,23 @@ import java.util.List;
 public class AddRequirementController {
 
     @GetMapping("/addRequirement")
-    public String showAddComponent() {
+    public String showAddComponent(Model model) {
+
+        model.addAttribute("requirements", requirementService.findAllRequirements());
+        model.addAttribute("components", componentService.findAllComponents());
         return "addRequirement";
     }
 
 
     @Autowired
     RequirementService requirementService;
-
     @Autowired
     ComponentRepository componentRepository;
 
     @Autowired
     RequirementRepository requirementRepository;
+    @Autowired
+    ComponentService componentService;
 
     @PostMapping("/addRequirement")
     public String add(@RequestParam(name = "componentId") int componentId,
