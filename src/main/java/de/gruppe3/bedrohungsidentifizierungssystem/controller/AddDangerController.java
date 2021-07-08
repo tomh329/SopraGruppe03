6acc8273323fe637462f5b9c80baa5c87eadd093
Tom Holzkämper <holzkaempertom@gmail.com>
2021-07-08 19:11:02 +0200

    addDanger optimized

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/AddDangerController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/AddDangerController.java
@@ -5,8 +5,10 @@ import de.gruppe3.bedrohungsidentifizierungssystem.entity.Requirement;
 import de.gruppe3.bedrohungsidentifizierungssystem.repository.DangerRepository;
 import de.gruppe3.bedrohungsidentifizierungssystem.repository.RequirementRepository;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.DangerService;
+import de.gruppe3.bedrohungsidentifizierungssystem.service.RequirementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +20,10 @@ public class AddDangerController {
 
 
     @GetMapping("/addDanger")
-    public String showAddDanger() {
+    public String showAddDanger(Model model) {
+
+        model.addAttribute("requirements", requirementService.findAllRequirements());
+        model.addAttribute("dangers", dangerService.findAllDangers());
         return "addDanger";
     }
 
@@ -29,6 +34,8 @@ public class AddDangerController {
     DangerRepository dangerRepository;
     @Autowired
     RequirementRepository requirementRepository;
+    @Autowired
+    RequirementService requirementService;
 
     @PostMapping("/addDanger")
     public String add(@RequestParam(name = "requirementId") int requirementId,
