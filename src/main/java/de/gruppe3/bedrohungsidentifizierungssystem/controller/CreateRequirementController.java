3d0d137298a9168abbb0701fc815f04bed5e9960
st168224 <st168224@stud.uni-stuttgart.de>
2021-07-14 13:43:33 +0200

    validation messages for the creation of entities

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/CreateRequirementController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/CreateRequirementController.java
@@ -1,18 +1,24 @@
 package de.gruppe3.bedrohungsidentifizierungssystem.controller;
 
+import de.gruppe3.bedrohungsidentifizierungssystem.entity.Requirement;
 import de.gruppe3.bedrohungsidentifizierungssystem.repository.DangerRepository;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.RequirementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class CreateRequirementController {
 
     @GetMapping("/createRequirement")
-    public String showCreateRequirement() {
+    public String showCreateRequirement(Requirement requirement, BindingResult bindingResult, Model model) {
+        model.addAttribute("requirement", requirement);
         return "createRequirement";
     }
 
@@ -23,7 +29,12 @@ public class CreateRequirementController {
     DangerRepository dangerRepository;
 
     @PostMapping("/createRequirement")
-    public String create(@RequestParam(name = "requirementName") String requirementName){
+    public String create(@RequestParam(name = "requirementName") String requirementName,
+                         @Valid Requirement requirement, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("requirement", requirement);
+            return "/createRequirement";
+        }
         requirementService.createRequirement(requirementName);
         return "redirect:/requirement";
     }
--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/CreateUserController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/CreateUserController.java
@@ -1,20 +1,24 @@
 package de.gruppe3.bedrohungsidentifizierungssystem.controller;
 
+import de.gruppe3.bedrohungsidentifizierungssystem.entity.User;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.RoleService;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class CreateUserController {
 
     @GetMapping("/createUser")
-    public String showCreateUser(Model model) {
-
+    public String showCreateUser(User user, BindingResult bindingResult, Model model) {
+        model.addAttribute("user", user);
         model.addAttribute("roles", roleService.findAllRoles());
         return "createUser";
     }
@@ -30,7 +34,13 @@ public class CreateUserController {
                          @RequestParam(name = "password") String password,
                          @RequestParam(name = "firstname") String firstname,
                          @RequestParam(name = "lastname") String lastname,
-                         @RequestParam(name = "roleId") int roleId) {
+                         @RequestParam(name = "roleId") int roleId,
+                         @Valid User user, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()){
+            model.addAttribute("user", user);
+            model.addAttribute("roles", roleService.findAllRoles());
+            return "/createUser";
+        }
         if(userService.findUserWithName(username) != null) {
             return "redirect:user";
         }
