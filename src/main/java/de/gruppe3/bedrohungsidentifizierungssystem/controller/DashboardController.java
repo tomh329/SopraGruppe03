e4d427b25d44ea59d16c63d34fbe1c90a800f6e4
JanBorufka <st166722@stud.uni-stuttgart.de>
2021-07-20 00:02:47 +0200

    Implemented first working statistics.

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/DashboardController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/DashboardController.java
@@ -1,26 +1,43 @@
 package de.gruppe3.bedrohungsidentifizierungssystem.controller;
 
+import de.gruppe3.bedrohungsidentifizierungssystem.service.ActionService;
+import de.gruppe3.bedrohungsidentifizierungssystem.service.ComponentService;
+import de.gruppe3.bedrohungsidentifizierungssystem.service.ProcessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DashboardController {
 
+    @Autowired
+    ActionService actionService;
+    @Autowired
+    ComponentService componentService;
+    @Autowired
+    ProcessService processService;
 
     @GetMapping("/dashboard")
-    public String showDashboard() {
+    public String showDashboard(Model model) {
+
+        String username;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        username = ((UserDetails)principal).getUsername();
+
+        int toDoActions = actionService.findToDoActionsForUser(username).size();
+        int doneActions = actionService.findDoneActionsForUser(username).size();
+        int countComponents = componentService.findAllComponents().size();
+        int countProcesses = processService.findAllProcesses().size();
+
+        model.addAttribute("toDoActions", Integer.toString(toDoActions));
+        model.addAttribute("doneActions", Integer.toString(doneActions));
+        model.addAttribute("countComponents", Integer.toString(countComponents));
+        model.addAttribute("countProcesses", Integer.toString(countProcesses));
         return "dashboard";
     }
 
-    @PostMapping("/process")
-    public String navigateToProcess() {
-        return "redirect:/process";
-    }
 
-
-    @PostMapping("/component")
-    public String navigateToComponent() {
-        return "redirect:/component";
-    }
 }
