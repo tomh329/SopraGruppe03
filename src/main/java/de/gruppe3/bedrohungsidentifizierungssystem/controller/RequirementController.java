e18c0456f8fcc58fb55fd24d2407f00bae3126da
Tom Holzkämper <holzkaempertom@gmail.com>
2021-07-20 23:33:16 +0200

    show the danger that is picked to add an requirement

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/RequirementController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/RequirementController.java
@@ -1,7 +1,11 @@
 package de.gruppe3.bedrohungsidentifizierungssystem.controller;
 
 
+import de.gruppe3.bedrohungsidentifizierungssystem.entity.Danger;
+import de.gruppe3.bedrohungsidentifizierungssystem.entity.Process;
+import de.gruppe3.bedrohungsidentifizierungssystem.repository.DangerRepository;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.ComponentService;
+import de.gruppe3.bedrohungsidentifizierungssystem.service.DangerService;
 import de.gruppe3.bedrohungsidentifizierungssystem.service.RequirementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +13,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.persistence.criteria.CriteriaBuilder;
 
 @Controller
 public class RequirementController {
@@ -17,6 +25,8 @@ public class RequirementController {
     private RequirementService requirementService;
     @Autowired
     ComponentService componentService;
+    @Autowired
+    DangerService dangerService;
 
     @GetMapping("/requirement")
     public String showRequirement(Model model) {
@@ -39,7 +49,11 @@ public class RequirementController {
     }
 
     @PostMapping("/addDangerButton")
-    public String add() {
+    public String add(@RequestParam(name = "dangerId") String dangerId,
+                      Model model, final RedirectAttributes redirectAttributes) {
+
+        Danger dangerToAdd = dangerService.findProcessWithId(Integer.parseInt(dangerId));
+        redirectAttributes.addFlashAttribute("dangerToAdd", dangerToAdd);
         return "redirect:/addDanger";
     }
 
