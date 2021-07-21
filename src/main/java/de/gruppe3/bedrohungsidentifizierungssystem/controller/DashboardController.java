2bdfa1a5c3a5b14768fe5bec3f25cf5345c7196f
Tom Holzkämper <holzkaempertom@gmail.com>
2021-07-21 15:32:40 +0200

    import update

--- src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/DashboardController.java
+++ src/main/java/de/gruppe3/bedrohungsidentifizierungssystem/controller/DashboardController.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
